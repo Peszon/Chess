b2555d8769b0b4282c4114367b7a728e3e6bb0b4
Peszon <Felix.b.persson@gmail.com>
2020-11-17 14:25:14 +0100

    added testing for movesets

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -5,13 +5,19 @@ require './lib/piece'
 require 'rspec'
 
 describe Piece do
-  let(:piece) { Piece.new('white', 'rook', proc { |x| [x + 1, x + 2, x + 3] }) }
+  let(:piece) { Piece.new('white', 'pawn') }
+
+  describe "::possible moves" do 
+    it "generates a proc that predicts how the piece moves" do
+      expect(Piece.get_moveset_proc("pawn")).to be_kind_of(Proc)
+      expect(piece.possible_moves.call([1,0])).to eq([[2,-1],[2,0],[2,1]])
+    end 
+  end 
 
   describe '#initalize' do
     it 'holds information about colour, subclass and possible moves' do
       expect(piece.colour).to eql('white')
       expect(piece.subclass).to eql('rook')
-      expect(piece.possible_moves.call(1)).to eql([2, 3, 4])
     end
   end
 end
