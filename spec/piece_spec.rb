2b86d6671701cc143c0c54e255526560590f3d5c
Peszon <Felix.b.persson@gmail.com>
2020-11-17 17:41:07 +0100

    formating

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -7,12 +7,12 @@ require 'rspec'
 describe Piece do
   let(:piece) { Piece.new('white', 'pawn') }
 
-  describe "::possible moves" do 
-    it "generates a proc that predicts how the piece moves" do
-      expect(Piece.get_moveset_proc("pawn")).to be_kind_of(Proc)
-      expect(piece.possible_moves.call([1,0])).to eq([[2,-1],[2,0],[2,1]])
-    end 
-  end 
+  describe '::possible moves' do
+    it 'generates a proc that predicts how the piece moves' do
+      expect(Piece.get_moveset_proc('pawn')).to be_kind_of(Proc)
+      expect(piece.possible_moves.call([1, 0])).to eq([[2, -1], [2, 0], [2, 1]])
+    end
+  end
 
   describe '#initalize' do
     it 'holds information about colour, subclass and possible moves' do
